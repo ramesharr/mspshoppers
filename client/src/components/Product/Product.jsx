dd17b7404d6fcedaebe5076c3771de928647ccff
ramesharr <ramesh1arr@gmail.com>
2018-12-04 06:37:56 +0530

    first commit

---
+++ client/src/components/Product/Product.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./Product.css";
+import PropTypes from 'prop-types';
+import DefaultThumb from './default-thumb.png';
+
+const Product = props => {
+  return (
+    <div className="Product-Wrapper">
+    <div className="Product">
+        <div className="Product-Image-Wrapper">
+          <img src={DefaultThumb} alt={props.name} className="Product-Image" />
+        </div>
+      <div className="Product-Title">
+        <p className="Product-Name">{props.name}</p>
+      </div>
+      <div className="Product-Weight">
+        <p className="Product-quantity">{props.quantity}(g)</p>
+      </div>
+      <div className="Product-Data">
+        <small className="Product-Price">${props.price}</small>
+        <button onClick={ props.addToCart } className="product-button Product-Add">Add to Cart</button>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  photo: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  addToCart: PropTypes.func.isRequired
+};
+
+export default Product;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGO_URL: 'mongodb://ramesh1arr:ramesh1arr@ds129821.mlab.com:29821/brownfox'
+};
